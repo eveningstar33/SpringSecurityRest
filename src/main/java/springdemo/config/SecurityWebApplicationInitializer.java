757741b0824c988a5df81a4ef21b311d45f3f1a3
Dragos Sarpe <contact@usdgadget.com>
2018-08-29 19:25:54 +0300

    Adding config package

---
+++ src/main/java/springdemo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package springdemo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+// This class enables the Spring Security filters for the web application
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
